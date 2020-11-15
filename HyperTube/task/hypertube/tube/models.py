dad496a96426ece5c9b7b023f2e182d6a69952eb
IgorBashkov <bashkov.torvm36@gmail.com>
2020-11-15 11:43:59 +0300

    Stage 1

--- HyperTube/task/hypertube/tube/models.py
+++ HyperTube/task/hypertube/tube/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Video(models.Model):
+    file = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+
+
+class VideoTag(models.Model):
+    tag = models.ForeignKey('tag', on_delete=models.CASCADE)
+    video = models.ForeignKey('video', on_delete=models.CASCADE)
